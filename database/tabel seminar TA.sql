d1cf36c2e41d2e1d03e59a5c75f1a248b1d4d76e
Regitha Zizilia <regithazizilia@gmail.com>
2024-01-08 11:43:25 +0700

    gabung database

--- database/tabel seminar TA.sql
+++ database/tabel seminar TA.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Des 2023 pada 02.54
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Generation Time: Jan 08, 2024 at 05:41 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tugas_akhir_ipl`
+-- Database: `tugasakhir2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `seminar ta1`
+-- Table structure for table `bimbingan`
 --
 
-CREATE TABLE `seminar ta1` (
+CREATE TABLE `bimbingan` (
+  `id_bimbingan` int(10) NOT NULL,
+  `nama_dsn` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login dosen`
+--
+
+CREATE TABLE `login dosen` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login mahasiswa`
+--
+
+CREATE TABLE `login mahasiswa` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seminarta1`
+--
+
+CREATE TABLE `seminarta1` (
   `id_seminar` int(10) NOT NULL,
   `NIM` int(10) NOT NULL,
   `nama_mhs` varchar(30) NOT NULL,
@@ -37,45 +70,204 @@ CREATE TABLE `seminar ta1` (
   `nilai_bimbingan` int(5) NOT NULL,
   `nilai_pengujian` int(5) NOT NULL,
   `nilai_seminar` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `seminar ta2`
+-- Table structure for table `seminarta2`
 --
 
-CREATE TABLE `seminar ta2` (
+CREATE TABLE `seminarta2` (
   `id_seminar` int(10) NOT NULL,
   `NIM` int(10) NOT NULL,
   `nama_mhs` varchar(30) NOT NULL,
-  `Judul` varchar(100) NOT NULL,
+  `judul` varchar(100) NOT NULL,
   `tema` varchar(100) NOT NULL,
   `nilai_adm` int(5) NOT NULL,
   `nilai_bimbingan` int(5) NOT NULL,
   `nilai_pengujian` int(5) NOT NULL,
   `nilai_seminar` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ta1`
+--
+
+CREATE TABLE `ta1` (
+  `NIM` int(10) NOT NULL,
+  `nama_mahasiswa` varchar(30) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `no_HP` int(12) NOT NULL,
+  `kelas` varchar(5) NOT NULL,
+  `angkatan` int(4) NOT NULL,
+  `jurusan` varchar(30) NOT NULL,
+  `fakultas` varchar(30) NOT NULL,
+  `tema` varchar(30) NOT NULL,
+  `judul` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ta2`
+--
+
+CREATE TABLE `ta2` (
+  `NIM` int(10) NOT NULL,
+  `nama_mhs` varchar(30) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `no_HP` int(12) NOT NULL,
+  `kelas` varchar(5) NOT NULL,
+  `angkatan` int(3) NOT NULL,
+  `jurusan` varchar(30) NOT NULL,
+  `fakultas` varchar(30) NOT NULL,
+  `tema` varchar(30) NOT NULL,
+  `judul` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jadwalbimbingan`
+--
+
+CREATE TABLE `tb_jadwalbimbingan` (
+  `kode_jadwal` int(10) NOT NULL,
+  `jam` time NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jadwalseminar`
+--
+
+CREATE TABLE `tb_jadwalseminar` (
+  `kode_jadwal` int(10) NOT NULL,
+  `NIM` int(10) NOT NULL,
+  `nama_mhs` varchar(30) NOT NULL,
+  `fakultas` varchar(30) NOT NULL,
+  `jurusan` varchar(30) NOT NULL,
+  `kelas` varchar(30) NOT NULL,
+  `jam` varchar(5) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelayakan`
+--
+
+CREATE TABLE `tb_kelayakan` (
+  `status_kelayakan` tinyint(1) DEFAULT NULL,
+  `NIM` int(10) NOT NULL,
+  `nama_mhs` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `seminar ta1`
+-- Indexes for table `bimbingan`
 --
-ALTER TABLE `seminar ta1`
-  ADD PRIMARY KEY (`id_seminar`),
-  ADD KEY `NIM` (`NIM`),
-  ADD KEY `judul` (`judul`),
-  ADD KEY `judul_2` (`judul`);
+ALTER TABLE `bimbingan`
+  ADD PRIMARY KEY (`id_bimbingan`);
 
 --
--- Indeks untuk tabel `seminar ta2`
+-- Indexes for table `login dosen`
 --
-ALTER TABLE `seminar ta2`
+ALTER TABLE `login dosen`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `login mahasiswa`
+--
+ALTER TABLE `login mahasiswa`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `seminarta1`
+--
+ALTER TABLE `seminarta1`
   ADD PRIMARY KEY (`id_seminar`),
-  ADD KEY `NIM` (`NIM`,`Judul`,`tema`);
+  ADD KEY `NIM` (`NIM`);
+
+--
+-- Indexes for table `seminarta2`
+--
+ALTER TABLE `seminarta2`
+  ADD PRIMARY KEY (`id_seminar`),
+  ADD KEY `NIM` (`NIM`);
+
+--
+-- Indexes for table `ta1`
+--
+ALTER TABLE `ta1`
+  ADD PRIMARY KEY (`NIM`);
+
+--
+-- Indexes for table `ta2`
+--
+ALTER TABLE `ta2`
+  ADD PRIMARY KEY (`NIM`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `tb_jadwalbimbingan`
+--
+ALTER TABLE `tb_jadwalbimbingan`
+  ADD PRIMARY KEY (`kode_jadwal`);
+
+--
+-- Indexes for table `tb_jadwalseminar`
+--
+ALTER TABLE `tb_jadwalseminar`
+  ADD PRIMARY KEY (`kode_jadwal`),
+  ADD KEY `NIM` (`NIM`);
+
+--
+-- Indexes for table `tb_kelayakan`
+--
+ALTER TABLE `tb_kelayakan`
+  ADD KEY `NIM` (`NIM`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `seminarta1`
+--
+ALTER TABLE `seminarta1`
+  ADD CONSTRAINT `seminarta1_ibfk_1` FOREIGN KEY (`NIM`) REFERENCES `ta1` (`NIM`);
+
+--
+-- Constraints for table `seminarta2`
+--
+ALTER TABLE `seminarta2`
+  ADD CONSTRAINT `seminarta2_ibfk_1` FOREIGN KEY (`NIM`) REFERENCES `ta2` (`NIM`);
+
+--
+-- Constraints for table `tb_jadwalseminar`
+--
+ALTER TABLE `tb_jadwalseminar`
+  ADD CONSTRAINT `tb_jadwalseminar_ibfk_1` FOREIGN KEY (`NIM`) REFERENCES `ta1` (`NIM`),
+  ADD CONSTRAINT `tb_jadwalseminar_ibfk_2` FOREIGN KEY (`NIM`) REFERENCES `ta2` (`NIM`);
+
+--
+-- Constraints for table `tb_kelayakan`
+--
+ALTER TABLE `tb_kelayakan`
+  ADD CONSTRAINT `tb_kelayakan_ibfk_1` FOREIGN KEY (`NIM`) REFERENCES `ta1` (`NIM`),
+  ADD CONSTRAINT `tb_kelayakan_ibfk_2` FOREIGN KEY (`NIM`) REFERENCES `ta2` (`NIM`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
